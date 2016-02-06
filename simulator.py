d0b64eac0e5985dabfb439336c7f03fbcc60e946
Bushwallyta271828 <bushwallyta@gmail.com>
2016-02-06 13:34:19 -0500

    Added converter from comp to slice.

--- simulator.py
+++ simulator.py
@@ -3,30 +3,29 @@ from compartmentalize import compartmentalize
 from pylab import *
 import numpy as np
 
+def comp_to_slices(compartmentalization):
+    slices = []
+    index = 0
+    for i in range(0, 1000, 100):
+        minimum = index
+        maximum = len(compartmentalization) - 1
+        while maximum - minimum > 1:
+            middle = (maximum + minimum) // 2
+            if compartmentalization[middle][0] < i:
+                minimum = middle
+            else:
+                maximum = middle
+        b = compartmentalization[minimum]
+        slices.append((b[1], b[2]))
+        index = minimum
+    return slices
+
 #def gen_exo():
     
 
 def gen_nox():
-    dat = np.random.poisson(900 * np.random.random() + 100,1000) #Note: to be made slightly more "wavy" with extra product.
-    comp = compartmentalize(dat)[0]
-    for inum, i in enumerate(comp[:-1]):
-        n = comp[inum + 1]
-        plot([i[0], n[0] - 1], [i[1], i[1]], "green")
-        plot([i[0], n[0] - 1], [i[2], i[2]], "red")
-    plot(dat, "blue")
-    show()
+    dat = np.random.poisson(900 * np.random.random() + 100,(5,1000)) #Note: to be made slightly more "wavy" with extra product.
+    return [comp_to_slices(compartmentalize(dat[i])[0]) for i in range(5)]
 
 if __name__=="__main__":
-    gen_nox()
-
-#a = [50*random() - 100 * 2.71828**(-(i / 50 - 2)**2) for i in range(200)]
-#comp = compartmentalize(a)[0]
-#for inum, i in enumerate(comp[:-1]):
-#    n = comp[inum + 1]
-#    sl = a[i:n]
-#    largest = max(sl)
-#    smallest = min(sl)
-#    plot([i, n], [largest, largest], "red")
-#    plot([i, n], [smallest, smallest], "green")
-#plot(range(200), a, "blue")
-#show()
+    print gen_nox()
